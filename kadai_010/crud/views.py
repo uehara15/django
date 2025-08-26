2fbc928f658700e70b2811b9491ef94c85c126d9
uehara15 <83382841+uehara15@users.noreply.github.com>
2025-08-26 20:26:59 +0900

    Update views.py

--- kadai_010/crud/views.py
+++ kadai_010/crud/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView, ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
-# Create your views here.
-class TopView(TemplateView):
-    template_name = "top.html"
 
 class ProductListView(ListView):
     model = Product
-    paginate_by = 3
-    template_name = "product_detail.html"
+    template_name = 'crud/product_list.html'
+    context_object_name = 'products'
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'crud/product_detail.html'
+    context_object_name = 'product'
